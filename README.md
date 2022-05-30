a35622c5a94bb407613bc2d80a437276e199b58f
Azizbekdev <azizbekqozoqovinfo@gmail.com>
2022-05-30 15:15:55 -0400

    Upgraded README.md

--- README.md
+++ README.md
@@ -98,8 +98,13 @@ dash-dot line style
 dotted line style
 ':'
 
+## Available styles 
+*plt.style.use("stylename")*
+
+'Solarize_Light2', '_classic_test_patch', '_mpl-gallery', '_mpl-gallery-nogrid', 'bmh', 'classic', 'dark_background', 'fast', 'fivethirtyeight', 'ggplot', 'grayscale', 'seaborn', 'seaborn-bright', 'seaborn-colorblind', 'seaborn-dark', 'seaborn-dark-palette', 'seaborn-darkgrid', 'seaborn-deep', 'seaborn-muted', 'seaborn-notebook', 'seaborn-paper', 'seaborn-pastel', 'seaborn-poster', 'seaborn-talk', 'seaborn-ticks', 'seaborn-white', 'seaborn-whitegrid', 'tableau-colorblind10'
 
 
 
-1. Basic plot **[basic_plot.py](./basic_plot.py)**
+
+Basic plot **[basic_plot.py](./basic_plot.py)**
 
--- basic_plot.py
+++ basic_plot.py
@@ -13,7 +13,7 @@ import tkinter.messagebox as mb
 # Use default style
 # plt.style.use("fivethirtyeight")
 
-plt.xkcd()
+# plt.xkcd()
 
 # X values - ages
 developers_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 45]
@@ -22,15 +22,18 @@ developers_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 45]
 py_devs = [35_500, 37_000, 40_000, 45_000, 50_000, 52_000, 55_500, 56_000, 59_000, 62_300, 63_000]
 js_devs = [28_000, 35_500, 37_000, 40_000, 45_000, 47_000, 52_000, 53_500, 55_000, 57_000, 64_000]
 
-# Create plots
-plt.plot(developers_x, py_devs, marker="o", markersize=6,linewidth=3, linestyle="--", color="b", label="Python developers")
-plt.plot(developers_x, js_devs, label="JavaScript developers", marker="*", markersize=6, linewidth=3, linestyle="--", color="y")
+all_devs = [28_000, 35_500, 37_000, 40_000, 45_000, 47_000, 52_000, 53_500, 55_000, 57_000, 64_000]
 
-# plt.grid(True) # Grid style
+# Create plots
+plt.plot(developers_x, py_devs, marker="o", markersize=6,linewidth=2, linestyle="--", color="b", label="Python developers")
+plt.plot(developers_x, js_devs, label="JavaScript developers", marker="*", markersize=6, linewidth=2, linestyle="--", color="y")
+
+plt.grid(True) # Grid style
 plt.legend()
 plt.xlabel("Ages")
 plt.ylabel("Salary")
 plt.title("Developers Salary by ages")
+
 
 if __name__ == "__main__":
     answer = mb.askyesno("Notice", "Let's create a program that shows salary of developers by age.")
