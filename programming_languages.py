1e31cf2dc70bc67d1cfad56745531cd23bfe1522
Azizbekdev <azizbekqozoqovinfo@gmail.com>
2022-06-11 14:05:00 +0500

    Upgraded for most popular programming languages

--- programming_languages.py
+++ programming_languages.py
@@ -6,37 +6,41 @@ data from https://www.statista.com/
 from matplotlib import pyplot as plt
 import numpy as np
 
+# print(plt.style.available)
+plt.style.use("ggplot")
+
 percentage = np.array([
-    26.78,
-    23.66,
-    22.15,
-    8.35,
-    7.09,
-    6.55,
-    5.33,
-    4.33,
+    3.43,
     3.74,
-    3.43
+    4.33,
+    5.33,
+    6.55,
+    7.09,
+    8.35,
+    22.15,
+    23.66,
+    26.78,
+    
 ])
 
 languages = np.array([
-    "Python",
-    "JavaScript",
-    "Java",
-    "C#",
-    "PHP",
-    "C++",
-    "C",
-    "R",
+    "Objective C",
     "Swift",
-    "Objective C"
+    "R",
+    "C",
+    "C++",
+    "PHP",
+    "C#",
+    "Java",
+    "JavaScript",
+    "Python",
 ])
 
-plt.bar(languages, percentage)
+plt.barh(languages, percentage, color='#2cd69a')
 
-plt.xlabel("Languages")
-plt.ylabel("Percentage")
 plt.title("The most popular programming languages in 2021 (TIOBE Index)")
+plt.xlabel("Percentage (%)")
+plt.ylabel("Languages")
 
 
 if __name__ == "__main__":
